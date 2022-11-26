0919b57ab8c0293fa136de1a090d2014e2bd1baf
Robin-0909114 <Winkels.r1@gmail.com>
2022-11-26 19:52:25 +0100

    Added show value into Pie chart

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def login():
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
-    return render_template('login.html', msg=msg)
+    return render_template('login.html', msg=msg,)
 
 
 
