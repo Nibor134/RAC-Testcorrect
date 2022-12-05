e190507d485047140a441669d9efcc5159926466
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2022-12-05 16:54:06 +0100

    Test CSV export

---
+++ CSV Export.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pandas as pd 
+#import sqlalchemy
+
+#SQL steps
+#select * from auteurs
+#select * from leerdoelen
+#select * from vragen
+
+connection = sqlite3.connect('testcorrect_vragen.db')
+cursor = connection.cursor()
+sqlquery = 'SELECT * FROM auteurs'
+result = cursor.fetchall()
+for row in result: 
+    df = pd.read_sql_query(sqlquery,connection)
+    df.to_csv('output.CSV', index = False)
